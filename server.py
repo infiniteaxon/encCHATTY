28d84fd310b7fa683b7c026a72978bbc55a629b4
axon <60622650+infiniteaxon@users.noreply.github.com>
2023-11-14 20:07:38 -0800

    Update server.py

--- server.py
+++ server.py
@@ -3,7 +3,6 @@ import threading
 import rsa
 from colorama import Fore, init
 
-
 init()  # Colorama
 colors = [Fore.BLUE, Fore.LIGHTBLUE_EX, Fore.CYAN, Fore.LIGHTCYAN_EX,
           Fore.GREEN, Fore.LIGHTGREEN_EX, Fore.RED, Fore.LIGHTRED_EX,
@@ -14,27 +13,9 @@ colors = [Fore.BLUE, Fore.LIGHTBLUE_EX, Fore.CYAN, Fore.LIGHTCYAN_EX,
 sHOST = socket.gethostbyname(socket.gethostname())
 sPORT = 2003
 
-# Open RSA Keys // Set up functions
-with open("server_public.pem", "rb") as f:
-    server_public = rsa.PublicKey.load_pkcs1(f.read())
 
-with open("server_private.pem", "rb") as f:
-    server_private = rsa.PrivateKey.load_pkcs1(f.read())
-
-with open("client_public.pem", "rb") as f:
-    client_public = rsa.PublicKey.load_pkcs1(f.read())
-
-
-def encryption_to_client(message):
-    if isinstance(message, str):
-        message = message.encode()
-    return rsa.encrypt(message, client_public)
-
-
-def decryption_from_client(message):
-    print(message)
-    decrypted_msg = rsa.decrypt(message, server_private)
-    return decrypted_msg.decode()
+server_public, server_private = rsa.newkeys(1024)
+client_keys = {}  # Dictionary to store public keys
 
 
 # Initiate Client Sockets
@@ -46,24 +27,39 @@ tcp.listen(5)  # Only 5 Connection can queue at once
 print(f"{Fore.GREEN}[*] Live on {sHOST}:{sPORT} {Fore.RESET}")  # Print Socket Connection Info
 
 
+# Function to broadcast messages to all clients
+def broadcast(message):
+    for clientSOCKET in cSOCKETS:
+        client_public_key = client_keys[clientSOCKET]
+        encrypted_msg = rsa.encrypt(message.encode(), client_public_key)
+        clientSOCKET.send(encrypted_msg)
+
+
 # Function to Listen for Client Connections
 def client_connect(cSOCKET):
     while True:
         try:
-            message = cSOCKET.recv(1024)  # Adjust buffer size as needed
-            decrypted_msg = decryption_from_client(message)
-        except Exception as e: # Error Handling
+            message = cSOCKET.recv(4096)  # Adjust buffer size as needed
+            decrypted_msg = rsa.decrypt(message, server_private).decode()
+            broadcast(decrypted_msg)
+        except Exception as e:  # Error Handling
             print(f"{Fore.RED}[!] Error: {e} {Fore.RESET}")
             cSOCKETS.remove(cSOCKET)  # Remove bad clients
-        for clientSOCKET in cSOCKETS:  # For loop to send message to all connected users
-            clientSOCKET.send(encryption_to_client(decrypted_msg))  # Send Message
+            broadcast(f"{Fore.YELLOW}[!] A user has disconnected.{Fore.RESET}")
+            cSOCKET.close()
+            break
 
 
+# Accept new client connections
 try:
     while True:
         cSOCKET, uADDRESS = tcp.accept()
         print(f"{Fore.GREEN}[+] {uADDRESS} connected. {Fore.RESET}")
         cSOCKETS.add(cSOCKET)
+        client_public_key_data = cSOCKET.recv(4096)  # Receive client's public key
+        client_public_key = rsa.PublicKey.load_pkcs1(client_public_key_data)
+        client_keys[cSOCKET] = client_public_key
+        cSOCKET.send(server_public.save_pkcs1('PEM'))  # Send server's public key to the client
         threading.Thread(target=client_connect, args=(cSOCKET,), daemon=True).start()
 except KeyboardInterrupt:
     print(f"{Fore.Yellow}[*] Server shutting down...{Fore.RESET}")
