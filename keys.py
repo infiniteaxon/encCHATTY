1990d175a6af96ba74be1544aa0d1b7c47d758c8
axon <60622650+infiniteaxon@users.noreply.github.com>
2023-11-14 20:05:55 -0800

    Delete keys.py

--- keys.py
+++
@@ -1,9 +0,0 @@
-import rsa
-
-
-def create_keys():
-    public_key, private_key = rsa.newkeys(1024)
-    with open("client_public.pem", "wb") as f:
-        f.write(public_key.save_pkcs1("PEM"))
-    with open("client_private.pem", "wb") as f:
-        f.write(private_key.save_pkcs1("PEM"))
